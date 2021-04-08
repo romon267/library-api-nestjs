ff632ec46187525460e71c5eef298e01d2c50504
unknown <romon267@gmail.com>
2021-04-08 12:02:28 +0300

    demo deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'You can use /users and /books routes, please refer to the docs on github.com/romon267';
   }
 }
